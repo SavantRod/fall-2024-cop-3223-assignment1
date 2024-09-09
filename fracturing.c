f9be8fa2e5540ad5bce6a2fbb91d9a6539011855
SavantRod <ro881804@ucf.edu>
2024-09-09 03:53:37 +0000

    Final edition

--- fracturing.c
+++ fracturing.c
@@ -2,11 +2,9 @@
 #include <math.h>  // to add math functions
 
 #define PI 3.14159
+double distanceValue(){
+    double x1, y1, x2, y2, xf, yf, distance;
 
-// to calculate the distance between two points
-double calculate_distance() {
-    double x1, y1, x2, y2, distance;
-    
     // Asking input from user
     printf("Enter x1: ");
     scanf("%lf", &x1);
@@ -17,48 +15,60 @@ double calculate_distance() {
     printf("Enter y2: ");
     scanf("%lf", &y2);
 
-    // calculating the distance between the points
-    distance = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+    // Printing out points
+    printf("Point #1 entered: %.2lf ; %.2lf\n",x1,y1);
+    printf("Point #2 entered: %.2lf; %.2lf\n",x2,y2);
     
-    printf("The distance between the two points is: %.2lf\n", distance);
+    // calculating the distance between the points
+    xf = (x2-x1);
+    yf = (y2-y1);
+    distance = sqrt(pow(xf, 2) + pow(xf, 2));
     
     return distance;
+
+}
+double calculateDistance() {
+    double calculated_distance = distanceValue();
+
+    printf("The distance between the two points is: %.2lf\n", calculated_distance);
+    
+    return calculated_distance;
 }
 
-// to calculate the perimeter of the circle (diameter)
-double calculate_perimeter() {
-    double distance = calculate_distance();
+// calculate the perimeter of the circle (diameter)
+double calculatePerimeter() {
+    double distance = distanceValue();
     double radius = distance / 2.0;
     double perimeter = 2 * PI * radius;
-    
-    printf("The perimeter of the city encompassed by your request is: %.2lf\n", perimeter);
+
+    printf("The perimeter of the city encompassed by your request is %.2lf\n", perimeter);
     
     return perimeter;
 }
 
-// to calculate the area of the circle (diameter)
-double calculate_area() {
-    double distance = calculate_distance();
+// calculate the area of the circle (diameter)
+double calculateArea() {
+    double distance = distanceValue();
     double radius = distance / 2.0;
     double area = PI * pow(radius, 2);
     
-    printf("The area of the city encompassed by your request is: %.2lf\n", area);
+    printf("The area of the city encompassed by your request is %.2lf\n", area);
     
     return area;
 }
 
-// to calculate the width of the city (diameter)
-double calculate_width() {
-    double distance = calculate_distance();
+// calculate the width of the city (diameter)
+double calculateWidth() {
+    double distance = distanceValue();
     
     printf("The width of the city encompassed by your request is: %.2lf\n", distance);
     
     return distance;
 }
 
-// to calculate the height of the city (diameter)
-double calculate_height() {
-    double distance = calculate_distance();
+// calculate the height of the city (diameter)
+double calculateHeight() {
+    double distance = distanceValue();
     
     printf("The height of the city encompassed by your request is: %.2lf\n", distance);
     
@@ -67,10 +77,11 @@ double calculate_height() {
 
 // Main function
 int main(void) {
-    calculate_perimeter();
-    calculate_area();
-    calculate_width();
-    calculate_height();
+    calculateDistance();
+    calculatePerimeter();
+    calculateArea();
+    calculateWidth();
+    calculateHeight();
     
     return 0;
 }
